9f61574a602d833c4bf2bde66200277d46d864ee
Josh <JoshKPrier@gmail.com>
2019-07-03 23:02:48 -0400

    Added PredictionModel

---
+++ PredictionModel.py
@@ -0,0 +1,11 @@
+import BikeShareController
+
+class PredictionModel():
+	def __init__(self):
+		self.parameters = BikeShareController.getTrainingParameters()
+
+	def setParameters(parameters):
+		self.parameters = parameters
+
+	def predict(data):
+		pass
