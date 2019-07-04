ff916265a1466c536ba8d9fe0c07e997e4e53e7d
Josh <JoshKPrier@gmail.com>
2019-07-03 22:52:21 -0400

    Added BikeShareController

---
+++ BikeShareController.py
@@ -0,0 +1,16 @@
+# From FrontEnd
+def predictUserInputData(data):
+	predictData(data)
+
+# From Prediction
+def getTrainingParameters():
+	#call training model to get parameters
+	#return parameters
+	pass
+
+### General Methods ###
+def predictData(data):
+	pass
+
+def trainData(data):
+	pass
